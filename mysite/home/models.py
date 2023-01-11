9034e204e430cca41a5fe6cd80b2c3744e73f82f
ali <ali.naqvi.dev@gmail.com>
2023-01-11 02:53:37 -0600

    created first fieldPanel on homepage

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -1,8 +1,24 @@
 from django.db import models
+from wagtail.admin.edit_handlers import FieldPanel
 
 from wagtail.core.models import Page
 
 
 class HomePage(Page):
-    
+    # associated template
     templates ="home/home_page/html"
+    
+    # there can only ever be one homepage instance at a time 
+    max_count = 1
+    
+    # blank is for form validation whereas null is for the database field
+    banner_title = models.CharField(max_length=100, blank=False, null=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("banner_title")
+    ]
+
+    class Meta:
+        # this will override 
+        verbose_name = "Home Page"
+        verbose_name_plural = 'Home Pages'
