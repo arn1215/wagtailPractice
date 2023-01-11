c2b2483b57e69ac69aceca3cc003ecf837d84404
ali <ali.naqvi.dev@gmail.com>
2023-01-11 02:21:57 -0600

    init

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-$2_6r1yma_#9(2wggn37m1)=eo*-u)425&@y87fbqsb^@)o4l#'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
